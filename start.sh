1c649c3cf5a19f6e6da2b99f869d9c615d3e4fe3
Hiavert <ucleshiaverth@gmail.com>
2025-08-05 00:23:18 -0600

    Mejorar start.sh: espera a DB antes de migrar y arrancar servidor

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sleep 10
+# Esperar a que la base de datos esté lista
+until nc -z -v -w30 $DB_HOST $DB_PORT
+do
+  echo "Esperando a la base de datos..."
+  sleep 5
+done
 
+# Ejecutar migraciones
 php artisan migrate --force || echo "Migraciones ya aplicadas o error ignorado"
 
+# Iniciar FrankenPHP
 SERVER_ADDR="0.0.0.0:$PORT" frankenphp php-server public/
+
