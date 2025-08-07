807e017f3018dc973aecd9370c64feb17d7d353e
Hiavert <ucleshiaverth@gmail.com>
2025-08-07 14:46:41 -0600

    Actualizar estilos del login y otras vistas

--- app/Models/AcuseTransferencia.php
+++ app/Models/AcuseTransferencia.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AcuseTransferencia extends Model
 {
-    use SoftDeletes;
     
     protected $table = 'acuses_transferencias';
     protected $primaryKey = 'id_transferencia';
